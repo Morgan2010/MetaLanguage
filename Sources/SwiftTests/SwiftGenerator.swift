a967104be1f7fda1a0ad25d5bd300c0150672da5
Morgan McColl <morgan.mccoll@alumni.griffithuni.edu.au>
2021-10-11 04:04:44 +1000

    Made basic swift generator, untested.

--- Sources/SwiftTests/SwiftGenerator.swift
+++ Sources/SwiftTests/SwiftGenerator.swift
@@ -7,10 +7,13 @@
 
 import Foundation
 import LLFSMTestingFramework
+import SwiftParsing
 
 struct SwiftGenerator {
     
-    public func generateWrapper(suite: TestSuite, fileName: String) -> FileWrapper? {
+    let mutator: StringMutator = StringMutator()
+    
+    public func generateWrapper(suite: TestSuite) -> FileWrapper? {
         guard
             let tests = generate(suite: suite),
             let data = tests.data(using: .utf8)
@@ -18,12 +21,63 @@ struct SwiftGenerator {
             return nil
         }
         let wrapper = FileWrapper(regularFileWithContents: data)
-        wrapper.preferredFilename = fileName
+        wrapper.preferredFilename = "\(suite.name).swift"
         return wrapper
     }
     
     public func generate(suite: TestSuite) -> String? {
-        return nil
+        let variables = suite.variables?.compactMap(toString).map { mutator.indentLines(data: $0) } ?? []
+        let setup = nil == suite.setup ? "" : mutator.indentLines(data: toString(setup: suite.setup!) ?? "")
+        let tests = suite.tests.compactMap(toString).map { mutator.indentLines(data: $0) }
+        guard tests.count > 0 else {
+            return nil
+        }
+        let variableCode = variables.reduce("") { mutator.joinWithNewLines(str1: $0, str2: $1, amount: 2) }
+        let testsCode = tests.reduce("") { mutator.joinWithNewLines(str1: $0, str2: $1, amount: 2) }
+        let header = "import Foundation\nimport XCTest"
+        return header + "\n\n" + "final class \(suite.name): XCTestCase " + mutator.createBlock(
+            for: variableCode + "\n\n" + setup + "\n\n" + testsCode
+        )
+    }
+    
+    private func toString(test: Test) -> String? {
+        switch test {
+        case .languageTest(let name, let code, let language):
+            switch language {
+            case .swift:
+                return "func \(name)() " + mutator.createBlock(for: code)
+            default:
+                return nil
+            }
+        default:
+            return nil
+        }
+    }
+    
+    private func toString(setup: Code) -> String? {
+        switch setup {
+        case .languageCode(let code, let language):
+            switch language {
+            case .swift:
+                return "override func setup() " + mutator.createBlock(for: code)
+            default:
+                return nil
+            }
+        default:
+            return nil
+        }
+    }
+    
+    private func toString(variable: Variable) -> String? {
+        switch variable {
+        case .languageVariable(_, let declaration, let language):
+            switch language {
+            case .swift:
+                return declaration
+            default:
+                return nil
+            }
+        }
     }
     
 }
