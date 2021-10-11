a0f5fb0a168d899ef03897ead6d968f2c0a26e96
Morgan McColl <morgan.mccoll@alumni.griffithuni.edu.au>
2021-10-12 02:45:36 +1000

    Added swift tests to library targets.

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "MetaLanguage",
-            targets: ["MetaLanguage"]),
+            targets: ["MetaLanguage", "SwiftTests"]),
     ],
     dependencies: [
         .package(name: "SwiftParsing", url: "https://github.com/Morgan2010/SwiftParsing.git", .branch("main"))
