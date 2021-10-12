cb9ecf10b2fe95d7058b25c86d1c1719f353d0a1
Morgan McColl <morgan.mccoll@alumni.griffithuni.edu.au>
2021-10-12 22:38:50 +1000

    Using ssh for dependencies for the moment.

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(name: "SwiftParsing", url: "https://github.com/Morgan2010/SwiftParsing.git", .branch("main"))
+        .package(name: "SwiftParsing", url: "ssh://git@github.com/Morgan2010/SwiftParsing.git", .branch("main"))
         
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
