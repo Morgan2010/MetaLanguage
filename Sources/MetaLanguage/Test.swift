09a480e431b78c5b51fbdc55d7b83531ef894bef
Morgan McColl <morgan.mccoll@griffithuni.edu.au>
2021-10-28 19:23:35 +1000

    Made Test conform to Codable.

--- Sources/MetaLanguage/Test.swift
+++ Sources/MetaLanguage/Test.swift
@@ -39,4 +39,50 @@ public enum Test {
     
 }
 
-extension Test: Codable, Equatable, Hashable {}
+extension Test: Equatable, Hashable {}
+
+extension Test: Codable {
+    
+    enum CodingKeys: CodingKey {
+        case languageTest
+    }
+
+    struct LanguageTestContainer: Codable {
+
+        let name: String
+
+        let code: String
+
+        let language: Language
+
+    }
+
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        guard let key = container.allKeys.first else {
+            fatalError("Failed to retrieve key")
+        }
+        switch key {
+        case .languageTest:
+            let testContainer = try container.decode(LanguageTestContainer.self, forKey: .languageTest)
+            self = .languageTest(
+                name: testContainer.name,
+                code: testContainer.code,
+                language: testContainer.language
+            )
+        }
+    }
+
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        switch self {
+        case .languageTest(let name, let code, let language):
+            try container.encode(
+                LanguageTestContainer(name: name, code: code, language: language),
+                forKey: .languageTest
+            )
+        }
+    }
+
+}
+
