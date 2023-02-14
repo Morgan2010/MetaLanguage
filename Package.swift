279aa9fc28b905291ff554b4b45382682372a7ad
Morgan McColl <morgan.mccoll@griffithuni.edu.au>
2023-02-14 19:17:04 +1000

    Bumped swift_helpers to 2.0.0.

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "git@github.com:Morgan2010/SwiftParsing.git", from: "0.1.0"),
-        .package(url: "https://github.com/mipalgu/swift_helpers.git", from: "1.0.2")
+        .package(url: "https://github.com/mipalgu/swift_helpers.git", from: "2.0.0")
         
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
